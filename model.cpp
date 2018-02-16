b10bbeef5e339ecbbadbf41fcc7cc2d956c77dce
Victor Yudin <victor@yudin.org>
2018-02-15 20:59:03 -0500

    Removed USD
    I need to figure out how to submit in to the store. I'll return USD
    later.

--- model.cpp
+++ model.cpp
@@ -1,388 +1,44 @@
 #include "model.h"
 
-#define _USE_MATH_DEFINES
-#include <math.h>
-
-#include <opensubdiv/far/primvarRefiner.h>
-#include <opensubdiv/far/topologyDescriptor.h>
-#include <pxr/base/gf/matrix4f.h>
-#include <pxr/base/js/json.h>
-#include <pxr/usd/usd/prim.h>
-#include <pxr/usd/usd/primRange.h>
-#include <pxr/usd/usd/stage.h>
-#include <pxr/usd/usdGeom/mesh.h>
-#include <QCoreApplication>
 #include <QDebug>
-#include <QDir>
 #include <QFile>
-
-PXR_NAMESPACE_USING_DIRECTIVE
-
-extern "C" bool GoodReadPlugInfoObject(
-    const std::string& pathname,
-    JsObject* result)
-{
-    result->clear();
-
-    QFile plugInfoFile(":/plugInfo.json");
-    if (!plugInfoFile.open(QIODevice::ReadOnly | QIODevice::Text))
-    {
-        qDebug("Can't read plugInfo.json");
-        return false;
-    }
-
-    // The Js library doesn't allow comments, but we'd like to allow them.
-    // Strip comments, retaining empty lines so line numbers reported in parse
-    // errors match line numbers in the original file content.
-    // NOTE: Joining a vector of strings and calling JsParseString() is *much*
-    // faster than writing to a stringstream and calling JsParseStream() as of
-    // this writing.
-    std::vector<std::string> filtered;
-    QTextStream stream(&plugInfoFile);
-    while (!stream.atEnd())
-    {
-        QString line = stream.readLine();
-        if (line.contains('#'))
-        {
-            line.clear();
-        }
-        filtered.push_back(line.toStdString());
-    }
-
-    // Read JSON.
-    JsParseError error;
-    JsValue plugInfo = JsParseString(TfStringJoin(filtered, "\n"), &error);
-
-    // Validate.
-    if (plugInfo.IsNull())
-    {
-        TF_RUNTIME_ERROR(
-            "Plugin info file %s couldn't be read "
-            "(line %d, col %d): %s",
-            pathname.c_str(),
-            error.line,
-            error.column,
-            error.reason.c_str());
-    }
-    else if (!plugInfo.IsObject())
-    {
-        // The contents didn't evaluate to a json object....
-        TF_RUNTIME_ERROR(
-            "Plugin info file %s did not contain a JSON object",
-            pathname.c_str());
-    }
-    else
-    {
-        *result = plugInfo.GetJsObject();
-    }
-    return true;
-}
-
-extern "C" bool (*_ReadPlugInfoObject)(
-    const std::string& pathname,
-    JsObject* result) = GoodReadPlugInfoObject;
-
-void GoodUSDDiagnostic(FILE* fout, const char* message)
-{
-    qDebug(message);
-}
-extern "C" void (*USDDiagnostic)(FILE* fout, const char* message) =
-    GoodUSDDiagnostic;
-
-struct Vertex
-{
-    // Minimal required interface for OpenSiubdiv
-    Vertex() {}
-
-    Vertex(Vertex const& src)
-    {
-        mPosition[0] = src.mPosition[0];
-        mPosition[1] = src.mPosition[1];
-        mPosition[2] = src.mPosition[2];
-    }
-
-    void Clear(void* = 0) { mPosition[0] = mPosition[1] = mPosition[2] = 0.0f; }
-
-    void AddWithWeight(Vertex const& src, float weight)
-    {
-        mPosition[0] += weight * src.mPosition[0];
-        mPosition[1] += weight * src.mPosition[1];
-        mPosition[2] += weight * src.mPosition[2];
-    }
-
-    void SetPosition(float x, float y, float z)
-    {
-        mPosition[0] = x;
-        mPosition[1] = y;
-        mPosition[2] = z;
-    }
-
-    const float* GetPosition() const { return mPosition; }
-
-private:
-    float mPosition[3];
-};
-
-// Returns the cross product of \p v1 and \p v2.
-inline void cross(float const* v1, float const* v2, float* vOut)
-{
-    vOut[0] = v1[1] * v2[2] - v1[2] * v2[1];
-    vOut[1] = v1[2] * v2[0] - v1[0] * v2[2];
-    vOut[2] = v1[0] * v2[1] - v1[1] * v2[0];
-}
-
-void osdSubdivide(
-    int iNverts,
-    int iNfaces,
-    float* iVerts,
-    int* iVertsperface,
-    int* iVertIndices,
-    int maxlevel,
-    VtIntArray& oFaceVertexCounts,
-    VtIntArray& oFaceVertexIndices,
-    VtArray<GfVec3f>& oPoints,
-    VtArray<GfVec3f>& oNormals)
-{
-    // Populate a topology descriptor with our raw data
-    typedef OpenSubdiv::Far::TopologyDescriptor Descriptor;
-
-    OpenSubdiv::Sdc::SchemeType type = OpenSubdiv::Sdc::SCHEME_CATMARK;
-
-    OpenSubdiv::Sdc::Options sdcOptions;
-    sdcOptions.SetVtxBoundaryInterpolation(
-        OpenSubdiv::Sdc::Options::VTX_BOUNDARY_EDGE_ONLY);
-
-    Descriptor desc;
-    desc.numVertices = iNverts;
-    desc.numFaces = iNfaces;
-    desc.numVertsPerFace = iVertsperface;
-    desc.vertIndicesPerFace = iVertIndices;
-
-    // Instantiate a FarTopologyRefiner from the descriptor
-    OpenSubdiv::Far::TopologyRefiner* refiner =
-        OpenSubdiv::Far::TopologyRefinerFactory<Descriptor>::Create(
-            desc,
-            OpenSubdiv::Far::TopologyRefinerFactory<Descriptor>::Options(
-                type, sdcOptions));
-
-    // Uniformly refine the topolgy up to 'maxlevel'
-    OpenSubdiv::Far::TopologyRefiner::UniformOptions options(maxlevel);
-
-    // Required by Limit()
-    options.fullTopologyInLastLevel = true;
-
-    // Uniformly refine the topology up to 'maxlevel'
-    refiner->RefineUniform(options);
-
-    // Allocate a buffer for vertex primvar data. The buffer length is set to
-    // be the sum of all children vertices up to the highest level of
-    // refinement.
-    std::vector<Vertex> vbuffer(refiner->GetNumVerticesTotal());
-    Vertex* verts = &vbuffer[0];
-
-    // Initialize coarse mesh positions
-    int nCoarseVerts = iNverts;
-    for (int i = 0; i < nCoarseVerts; ++i)
-    {
-        verts[i].SetPosition(
-            iVerts[i * 3 + 0], iVerts[i * 3 + 1], iVerts[i * 3 + 2]);
-    }
-
-    // Interpolate vertex primvar data
-    OpenSubdiv::Far::PrimvarRefiner primvarRefiner(*refiner);
-
-    Vertex* src = verts;
-    for (int level = 1; level <= maxlevel; ++level)
-    {
-        Vertex* dst = src + refiner->GetLevel(level - 1).GetNumVertices();
-        primvarRefiner.Interpolate(level, src, dst);
-        src = dst;
-    }
-
-    // Limit position, derivatives and normals
-    int nFineVerts = refiner->GetLevel(maxlevel).GetNumVertices();
-    std::vector<Vertex> fineLimitPos(nFineVerts);
-    std::vector<Vertex> fineDu(nFineVerts);
-    std::vector<Vertex> fineDv(nFineVerts);
-
-    primvarRefiner.Limit(src, fineLimitPos, fineDu, fineDv);
-
-    {
-        OpenSubdiv::Far::TopologyLevel const& refLastLevel =
-            refiner->GetLevel(maxlevel);
-
-        int nverts = refLastLevel.GetNumVertices();
-        int nfaces = refLastLevel.GetNumFaces();
-
-        // Print vertex positions
-        int firstOfLastVerts = refiner->GetNumVerticesTotal() - nverts;
-
-        oPoints.resize(nverts);
-        oNormals.resize(nverts);
-
-        for (int vert = 0; vert < nverts; ++vert)
-        {
-            float const* pos = verts[firstOfLastVerts + vert].GetPosition();
-            oPoints[vert] = GfVec3f(pos[0], pos[1], pos[2]);
-
-            float norm[3];
-            float const* du = fineDu[vert].GetPosition();
-            float const* dv = fineDv[vert].GetPosition();
-
-            cross(du, dv, norm);
-            oNormals[vert] = GfVec3f(norm[0], norm[1], norm[2]);
-        }
-
-        oFaceVertexCounts.resize(nfaces);
-
-        int nIndices = 0;
-        for (int face = 0; face < nfaces; ++face)
-        {
-            OpenSubdiv::Far::ConstIndexArray fverts =
-                refLastLevel.GetFaceVertices(face);
-
-            // All refined Catmark faces should be quads
-            assert(fverts.size() == 4);
-            oFaceVertexCounts[face] = fverts.size();
-            nIndices += fverts.size();
-        }
-
-        oFaceVertexIndices.resize(nIndices);
-
-        int counter = 0;
-        for (int face = 0; face < nfaces; ++face)
-        {
-            OpenSubdiv::Far::ConstIndexArray fverts =
-                refLastLevel.GetFaceVertices(face);
-
-            for (int vert = 0; vert < fverts.size(); ++vert)
-            {
-                oFaceVertexIndices[counter++] = fverts[vert];
-            }
-        }
-    }
-}
+#include <QStandardPaths>
 
 Model::Model(const char* iFile)
 {
-    QFile modelFile(iFile);
+    QFile modelDataFile(QString(iFile) + ".data.cache");
 
-    if (!modelFile.open(QIODevice::ReadOnly | QIODevice::Text))
+    if (!modelDataFile.open(QIODevice::ReadOnly | QIODevice::Text))
     {
         qDebug("Can't read file");
         qDebug(iFile);
         return;
     }
 
-    SdfLayerRefPtr layer = SdfLayer::CreateAnonymous(iFile);
-    layer->ImportFromString(modelFile.readAll().constData());
+    QFile modelIndexFile(QString(iFile) + ".index.cache");
 
-    UsdStageRefPtr stage = UsdStage::Open(layer);
-    if (!TF_VERIFY(stage, "Can't open %s", iFile))
+    if (!modelIndexFile.open(QIODevice::ReadOnly | QIODevice::Text))
     {
+        qDebug("Can't read file");
+        qDebug(iFile);
         return;
     }
 
-    UsdPrimRange range(stage->GetPseudoRoot());
-    for (const UsdPrim& prim : range)
+    QTextStream modelDataStream(&modelDataFile);
+    while (!modelDataStream.atEnd())
     {
-        if (!prim.IsA<UsdGeomMesh>())
-        {
-            continue;
-        }
-
-        qDebug(prim.GetPath().GetText());
-
-        // Get the mesh.
-        UsdGeomMesh mesh(prim);
-
-        // Get the transform.
-        GfMatrix4f matrix(mesh.ComputeLocalToWorldTransform(0.0));
-        int pointsStartFrom = mData.size() / 9;
-
-        // Get the mesh data.
-        VtIntArray faceVertexCounts;
-        mesh.GetFaceVertexCountsAttr().Get(&faceVertexCounts);
-
-        VtIntArray faceVertexIndices;
-        mesh.GetFaceVertexIndicesAttr().Get(&faceVertexIndices);
-
-        VtArray<GfVec3f> points;
-        mesh.GetPointsAttr().Get(&points);
-
-        VtArray<GfVec3f> normals;
-
-        // Subdivide it.
-        osdSubdivide(
-            points.size(),
-            faceVertexCounts.size(),
-            reinterpret_cast<float*>(points.data()),
-            faceVertexCounts.data(),
-            faceVertexIndices.data(),
-            1,
-            faceVertexCounts,
-            faceVertexIndices,
-            points,
-            normals);
-
-        VtArray<GfVec3f> color;
-        mesh.GetDisplayColorAttr().Get(&color);
-
-        mData.reserve(mData.size() + points.size() * 3 * 3);
-        for (int i = 0; i < points.size(); i++)
-        {
-            const GfVec3f pt = matrix.Transform(points[i]);
-            const GfVec3f nt = matrix.Transform(normals[i]);
-            mData.push_back(pt[0]);
-            mData.push_back(pt[1]);
-            mData.push_back(pt[2]);
-            mData.push_back(nt[0]);
-            mData.push_back(nt[1]);
-            mData.push_back(nt[2]);
-            mData.push_back(color[0][0]);
-            mData.push_back(color[0][1]);
-            mData.push_back(color[0][2]);
-        }
-
-        int numTriangles = 0;
-        for (int n : faceVertexCounts)
-        {
-            if (n < 3)
-            {
-                continue;
-            }
-
-            numTriangles += n - 3;
-        }
-
-        mIndexData.reserve(mIndexData.size() + numTriangles * 3);
-
-        int counter = 0;
-        for (int n : faceVertexCounts)
-        {
-            for (int i = 2; i < n; i++)
-            {
-#if 1
-                mIndexData.push_back(
-                    pointsStartFrom + faceVertexIndices[counter + i]);
-                mIndexData.push_back(
-                    pointsStartFrom + faceVertexIndices[counter + i - 1]);
-                mIndexData.push_back(
-                    pointsStartFrom + faceVertexIndices[counter]);
-#else
-                // Reversed for tests
-                mIndexData.push_back(
-                    pointsStartFrom + faceVertexIndices[counter]);
-                mIndexData.push_back(
-                    pointsStartFrom + faceVertexIndices[counter + i - 1]);
-                mIndexData.push_back(
-                    pointsStartFrom + faceVertexIndices[counter + i]);
-#endif
-            }
-
-            counter += n;
-        }
+        QString line = modelDataStream.readLine();
+        mData.push_back(line.toFloat());
     }
+    modelDataFile.close();
+
+    QTextStream modelIndexStream(&modelIndexFile);
+    while (!modelIndexStream.atEnd())
+    {
+        QString line = modelIndexStream.readLine();
+        mIndexData.push_back(line.toInt());
+    }
+    modelIndexFile.close();
 
     return;
 }
